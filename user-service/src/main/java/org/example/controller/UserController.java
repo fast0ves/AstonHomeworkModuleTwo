d6239e698bd7a02fef8c937a021c5a63bcc8d8b5
Andrey Shorokhov <andreiv_shorokhov@mail.ru>
2025-11-12 19:01:05 +0500

    refactor hateoas

--- user-service/src/main/java/org/example/controller/UserController.java
+++ user-service/src/main/java/org/example/controller/UserController.java
@@ -3,18 +3,19 @@ package org.example.controller;
 import org.example.dto.UserRequestDto;
 import org.example.dto.UserResponseDto;
 import org.example.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.Map;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("/api/users")
-@Tag(name = "User API", description = "API для управления пользователями")
 public class UserController {
 
     private final UserService userService;
@@ -24,40 +25,55 @@ public class UserController {
         this.userService = userService;
     }
 
-    @Operation(summary = "Получить пользователя по ID",
-    description = "Позволяет получить пользователя по ID")
+    @Operation(summary = "Получить пользователя по ID", description = "Позволяет получить пользователя по ID")
     @GetMapping("/{id}")
     public UserResponseDto getUser(@PathVariable int id) {
-        return userService.findUserById(id);
+        UserResponseDto user = userService.findUserById(id);
+
+        user.add(linkTo(methodOn(UserController.class).getUser(id)).withSelfRel());
+        user.add(linkTo(methodOn(UserController.class).updateUser(id, null)).withRel("update"));
+        user.add(linkTo(methodOn(UserController.class).deleteUser(id)).withRel("delete"));
+        user.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("collection"));
+
+        return user;
     }
 
-    @Operation(summary = "Создать нового пользователя",
-    description = "Позволяет создать нового пользователя")
+
+    @Operation(summary = "Создать нового пользователя", description = "Позволяет создать нового пользователя")
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto userRequestDto) {
         UserResponseDto createdUser = userService.createUser(userRequestDto);
+
+        createdUser.add(linkTo(methodOn(UserController.class).getUser(createdUser.getId())).withSelfRel());
+        createdUser.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("collection"));
+
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
-    @Operation(summary = "Обновить пользователя",
-    description = "Позволяет обновить пользователя")
+
+    @Operation(summary = "Обновить пользователя", description = "Позволяет обновить пользователя")
     @PutMapping("/{id}")
     public UserResponseDto updateUser(@PathVariable int id, @RequestBody UserRequestDto userRequestDto) {
-        return userService.updateUser(id, userRequestDto);
+        UserResponseDto updatedUser = userService.updateUser(id, userRequestDto);
+
+        updatedUser.add(linkTo(methodOn(UserController.class).getUser(id)).withSelfRel());
+        updatedUser.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("collection"));
+
+        return updatedUser;
     }
 
-    @Operation(summary = "Удалить пользователя",
-    description = "Позволяет удалить пользователя")
+    @Operation(summary = "Удалить пользователя", description = "Позволяет удалить пользователя")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable int id) {
         userService.deleteUser(id);
+
         return ResponseEntity.noContent().build();
     }
 
-    @Operation(summary = "Получить информацию о API",
-    description = "Получение информации об API")
+    @Operation(summary = "Получить информацию о API", description = "Получение информации об API")
     @GetMapping
     public Map<String, String> getAllUsers() {
+
         return Map.of(
                 "info", "User Management API",
                 "endpoints", "GET/POST /api/users, GET/PUT/DELETE /api/users/{id}",
